bf301f17770b5ebc8c796c1e76a9c6c6ea1e61a7
Carolinemay <czhou@students.chccs.k12.nc.us>
2018-07-27 14:36:16 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,35 @@
 # Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end 
+  def brand 
+  @brand 
+end
+  def color
+    @color
+  end
+  def color=(color="red")
+    @color = color
+  end
+  def size 
+    @size
+end
+  def size=(size="")
+    @size=size 
+end
+  def material
+    @material
+  end
+  def material=(material="")
+    @material = material
+  end
+  def condition 
+    @condition
+  end
+  def condition=(condition = "tattered")
+    @condition =  condition
+  end
+end
+shoe1 = Shoe.new("Nike")
+    
